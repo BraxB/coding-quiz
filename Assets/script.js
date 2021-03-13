85688a1b263caee76d721617c334aac7e04ba7cb
BraxB <braxb92@gmail.com>
2021-03-13 14:24:45 -0500

    began working on issue with timer continuing to count to negatives rather than triggering endGame function... not currently working right

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,3 @@
-// TODO build out if statement for end of game
-
 var startEl = document.getElementById('start-btn');
 var questionEl = document.getElementById('question');
 var answerEl = document.getElementById('answer-grid');
@@ -15,30 +13,42 @@ timerEl.textContent = timer;
 // when clicked the start button hides itself and begins displaying the question and answer elements
 startEl.addEventListener('click', startGame);
 
-//function that displays the q&a objects and initializes timer after 
+//function that displays the q&a objects and initializes timer after start is pressed. 
+// SHOULD stop and break out when the timer reaches 0, but it keeps going to negative for some reason
 function startGame() {
     startEl.classList.add('hide');
     answerEl.style.display = 'grid';
-    setInterval(function(){ 
-        timer--; 
-        timerEl.textContent = timer;
-    }, 1000);
     score = 0;
+    if (timer > 0) {
+        setInterval(function(){ 
+            timer--; 
+            timerEl.textContent = timer;
+        }, 1000)
+    }
+    else {
+        endGame();
+    }
     setQuestion(qIndex);
 }
 
+//sets the next question or ends the game if the next question is undefined
 function setQuestion(question) {
-    questionEl.textContent = questions[qIndex].question;
-    questions[qIndex].answers.forEach(answer => {
-        var button = document.createElement('button');
-        button.innerText = answer.text;
-        button.classList.add('btn');
-        if (answer.correct) {
-            button.dataset.correct = answer.correct;
-        }
-        button.addEventListener('click', selectAnswer);
-        answerEl.appendChild(button);
-    })
+    if (questions[qIndex] === undefined) {
+        endGame();
+    }
+    else {
+        questionEl.textContent = questions[qIndex].question;
+        questions[qIndex].answers.forEach(answer => {
+            var button = document.createElement('button');
+            button.innerText = answer.text;
+            button.classList.add('btn');
+            if (answer.correct) {
+                button.dataset.correct = answer.correct;
+            }
+            button.addEventListener('click', selectAnswer);
+            answerEl.appendChild(button);
+        })
+    }
 }
 
 // this function removes answers from the previous question and must be called whenever a new q&a object is set
@@ -66,9 +76,13 @@ function selectAnswer(e) {
 }
 
 // the following code will end the game when conditions are met
-// if (qIndex > questions.length || timer = 0) {
-    
-// }
+function endGame() {
+    resetAnswers();
+    questionEl.textContent = "Great job! Type your initials below to log your score.";
+    var initials = document.createElement('input');
+    answerEl.appendChild(initials);
+    timer = 75;
+}
 
 // created an array of objects for each question
 var questions = [
